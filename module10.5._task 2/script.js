5e6dfcf8b7af59799a3837fd882634214ad577f4
ArturAminev <107628813+ArturAminev@users.noreply.github.com>
2022-09-25 16:52:55 +0300

    Add files via upload

---
+++ module10.5._task 2/script.js
@@ -0,0 +1,6 @@
+ const windowOuterWidth = window.outerWidth;
+const windowOuterHeight = window.outerHeight;//Собственно сам скрипт вывода размера окна браузера//
+function alerted(){
+alert( 'Размер экрана' +  window.outerWidth + 'x' +  window.outerHeight);
+};
+
